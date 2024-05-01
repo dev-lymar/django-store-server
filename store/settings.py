6c62d698a1f435bc3c0ad789830b90ee7bfaebde
Lymar Volodymyr <shaxtar11@gmail.com>
2024-05-01 20:34:06 +0200

    create users app, add user model, admin, auth_user_model in settings, fixtures goods.json

--- store/settings.py
+++ store/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'products'
+    'products',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Users
+
+AUTH_USER_MODEL = 'users.User'
