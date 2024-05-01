cee080ab3dd82e7c78972d931266cde459b63673
Lymar Volodymyr <shaxtar11@gmail.com>
2024-05-01 23:58:18 +0200

    added the ability to edit your profile

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 
 from .models import User
-from .forms import UserLoginForm, UserRegistrationForm
+from .forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -32,5 +32,17 @@ def registration(request):
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    contest = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', contest)
 
 
