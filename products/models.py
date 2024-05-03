8c2bb9b8e05a725b5480cd9c9a0458be5b474d82
Lymar Volodymyr <shaxtar11@gmail.com>
2024-05-03 16:38:04 +0200

    editing django admin panel

--- products/models.py
+++ products/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 from users.models import User
 
+
 class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
@@ -17,6 +22,10 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
@@ -37,6 +46,10 @@ class Basket(models.Model):
     created_timestamp = models.DateTimeField(auto_now_add=True)
 
     objects = BasketQuerySet.as_manager()
+
+    class Meta:
+        verbose_name = 'Basket'
+        verbose_name_plural = 'Baskets'
 
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
