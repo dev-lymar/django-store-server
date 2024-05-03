fb0b41f60b254f614f68292781f56a60d06d4bb2
Lymar Volodymyr <shaxtar11@gmail.com>
2024-05-03 18:45:43 +0200

    create emailverification model, migrations, register in admin panel

--- users/models.py
+++ users/models.py
@@ -4,7 +4,18 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
+    is_verified_email = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return f'EmailVerification object for {self.user.email}'
