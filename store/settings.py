17764ff9c3bbbde0e0c2e3f546c6657adf7f7705
Lymar Volodymyr <shaxtar11@gmail.com>
2024-05-05 14:02:49 +0200

    added products caching, updated requirements

--- store/settings.py
+++ store/settings.py
@@ -93,6 +93,13 @@ INTERNAL_IPS = [
     '127.0.0.1', 'localhost',
 ]
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
