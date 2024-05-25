c5d35531d0ec1e4645cf60532a283019a7c27f6c
Lymar Volodymyr <shaxtar11@gmail.com>
2024-05-25 09:32:38 +0200

    fix script, update env.example

--- create_superuser.py
+++ create_superuser.py
@@ -1,22 +1,22 @@
 import os
+
 import django
 import environ
-
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
 django.setup()
 
 env = environ.Env(
-    USERNAME=(str),
-    EMAIL=(str),
-    PASSWORD=(str),
+    SUPERUSER_USERNAME=(str),
+    SUPERUSER_EMAIL=(str),
+    SUPERUSER_PASSWORD=(str),
 )
 
 User = get_user_model()
-username = env("USERNAME")
-email = env("EMAIL")
-password = env("PASSWORD")
+username = env("SUPERUSER_USERNAME")
+email = env("SUPERUSER_EMAIL")
+password = env("SUPERUSER_PASSWORD")
 
 if not User.objects.filter(username=username).exists():
     print(f"Creating superuser: {username}")
