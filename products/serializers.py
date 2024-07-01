cb795960bd2dbea305943f68ea881f8249261cab
Lymar Volodymyr <shaxtar11@gmail.com>
2024-07-01 18:39:28 +0200

    added create method in api basket model viewset

--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,6 @@
-from rest_framework import serializers
-from rest_framework import fields
+from rest_framework import fields, serializers
 
-from products.models import Product, ProductCategory, Basket
+from products.models import Basket, Product, ProductCategory
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -14,7 +13,7 @@ class ProductSerializer(serializers.ModelSerializer):
 
 class BasketSerializer(serializers.ModelSerializer):
     product = ProductSerializer()
-    sum = fields.FloatField()
+    sum = fields.FloatField(required=False)
     total_sum = fields.SerializerMethodField()
     total_quantity = fields.SerializerMethodField()
 
